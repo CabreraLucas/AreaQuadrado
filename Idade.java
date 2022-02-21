c5d6293acfd64ac0470efbca40d3301a1e3ed7aa
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-02-21 19:42:09 +0000

    Idade

--- Idade.java
+++ Idade.java
@@ -1,6 +1,9 @@
 public class Idade {
     public static void main(String[] args){
         int idade=38;
-        System.out.println("Fulano tem"+ idade +"anos");
+        double media= 7.55;
+
+        System.out.println("Fulano tem " +idade+ " anos");
+        System.out.printf("Média = %.2f\n", media);
     }
 }
