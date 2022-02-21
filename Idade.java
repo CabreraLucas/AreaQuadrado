651c7f8ba15f0185569e458d32f48703f0078d5b
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-02-21 19:54:49 +0000

    Idade

--- Idade.java
+++ Idade.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class Idade {
     public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
         int idade=38;
         double media= 7.55;
+        int num;
 
         System.out.println("Fulano tem " +idade+ " anos");
         System.out.printf("Média = %.2f\n", media);
+        System.out.println("Digite um valor inteiro: ");
+        num = entrada.nextInt();
     }
 }
